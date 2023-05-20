5c1426015bc0d1433b09f137cec3c746ebae8219
Jose Torres <vidojam@gmail.com>
2023-05-20 15:27:49 -0400

    Remove index,js local

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import { getAllEvals, addEval, deleteEval, updateEval, getOneEval} from "./src/evals.js";
 
-const PORT = 3002;
+//const PORT = 3002;
 
 const app = express();
 
@@ -17,9 +17,9 @@ app.post("/evals", addEval);
 app.delete("/evals/:docId", deleteEval);
 app.patch("/evals/:docId", updateEval);
 
-app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}...`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Listening on http://localhost:${PORT}...`);
+// });
 
 export const api = functions.https.onRequest(app);
 
